5f6c1948f0af434112ef036c36819ee1828a1efb
fmowl10 <jinseok1001@hotmail.com>
2018-09-12 13:02:44 +0900

    replace code fibo to recbinsearch

--- assn2/assignment5-20181600/assignment5_20181600-2.py
+++ assn2/assignment5-20181600/assignment5_20181600-2.py
@@ -1,31 +1,58 @@
 import time
+import random
 
-def fibo(n):
-    if n <= 1:
-        return n
-    return fibo(n - 1) + fibo(n - 2)
 
-def iterfibo(n):
-    if n in [0, 1]:
-        return n
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
 
-    i, j = 0, 1
-    for _ in range(2, n + 1):
-        i, j = j, i + j
 
-    return j
+def recbinsearch(L, l, u, target):
+    if l > u:
+        return -1
 
-while True:
-    nbr = int(input("Enter a number: "))
-    if nbr == -1:
-        break
+    mid = (l + u) // 2
 
-    ts = time.time()
-    fibonumber = iterfibo(nbr)
-    ts = time.time() - ts
-    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    if L[mid] == target:
+        return mid
+    elif L[mid] > target:
+        return recbinsearch(L, l, mid - 1, target)
+    else:
+        return recbinsearch(L, mid + 1, u, target)
 
-    ts = time.time()
-    fibonumber = fibo(nbr)
-    ts = time.time() - ts
-    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
